04bbc33c987099e9d8882122851f966d6d52afda
Bastefer <105882186+Bastefer@users.noreply.github.com>
2022-11-08 21:18:23 +0300

    Add files via upload

---
+++ laba8.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+
+'''1'''
+print(np.zeros(15))
+'''2'''
+# print(np.full(8,3.2))
+'''3'''
+# m = np.zeros(15)
+# m[4] = '5'
+# print(m)
+'''4'''
+# print(np.arange(12, 44))
+'''5'''
+# print(np.random.random((3,3,2)))
+'''6'''
+# m = np.random.random((12,12))
+# print(m.max())
+# print(m.min())
+# print((m))
+'''7'''
+# m = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=int)
+# print((m))
+'''8'''
+# m = np.zeros((8,8), dtype=int)
+# m[1::2,0::2]= 1
+# m[0::2, 1::2]= 1
+# print(m)
+'''9'''
+# print(np.tile(np.array([[0,1],[1,0]]), (4,4)))
+'''10'''
+# print(np.dot(np.random.random((4,2)),np.random.random((2,5))))
+'''11'''
+# m = np.arange(11)
+# m[(3 < m) & (m < 7)] *= -1
+# print(m)
+
+
